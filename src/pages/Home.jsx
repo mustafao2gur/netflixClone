cbe20ddca00d77a027f12ad9f64f994a3cd51109
mustafao2gur <mustafaseviss@gmail.com>
2023-03-17 22:50:55 +0300

    env

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import Main from '../Components/main/Main'
+import Row from '../Components/row/Row'
+import info from "../info"
 
 const Home = () => {
   return (
     <>
-    <Main/>
+      <Main />
+      <Row title="Up Coming" fetchURL={info.requestUpcoming} />
+      <Row title="Popular" fetchURL={info.requestPopular} />
+      <Row title="Trending" fetchURL={info.requestUpcoming} />
+      <Row title="Top Rated" fetchURL={info.requestTopRated} />
+      <Row title="Horror" fetchURL={info.requestHorror} />
     </>
-  )
+  );
 }
 
 export default Home
